914fb4e028a66b0cf2f3c0193ce51fe05b423e5f
dzakki <dzakkiaz7@gmail.com>
2019-10-25 10:38:47 +0700

    remove function excercise-3

--- excercise-3.js
+++ excercise-3.js
@@ -2,36 +2,33 @@
     // peran = Ksatria, Tabib, Penyihir
     var nama = 'Dzakki'
     var peran = 'Ksatria'
-    Proxytia(nama, peran)
-    function Proxytia(nama, peran) {
-        if (nama.trim() != '') {
-            if (peran.trim() != '') {
-                if (nama == 'Dzakki') {
-                    if (peran == 'Ksatria') {
-                        console.log(`"Selamat datang di Dunia Proxytia, ${nama} " \n "Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu! "`)
-                        console.log('selesai')
-                    } else {
-                        console.log('"Peran salah, silahkan coba lagi!"')
-                    }
-                }else if (nama == 'Bagus') {
-                    if (peran == 'Tabib') {
-                        console.log(`"Selamat datang di Dunia Proxytia, ${nama}" \n "Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka." `)
-                    } else {
-                        console.log('"Peran salah, silahkan coba lagi!"')
-                    }
-                }else if (nama == 'Rendy') {
-                    if (peran == 'Penyihir') {
-                        console.log(`"Selamat datang di Dunia Proxytia, ${nama}" \n Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
-                    } else {
-                        console.log('"Peran salah, silahkan coba lagi!"')
-                    }
-                }else{
-                    console.log('"nama tidak ditemukan, silahkan coba lagi!"')
-                }   
-            } else {
-                console.log(`Hallo ${nama} Pilih peranmu untuk memulai game!`)
-            }
-        }else{
-            console.log(' "Nama harus diisi" ');
+    if (nama.trim() != '') {
+        if (peran.trim() != '') {
+            if (nama == 'Dzakki') {
+                if (peran == 'Ksatria') {
+                    console.log(`"Selamat datang di Dunia Proxytia, ${nama} " \n "Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu! "`)
+                    console.log('selesai')
+                } else {
+                    console.log('"Peran salah, silahkan coba lagi!"')
+                }
+            }else if (nama == 'Bagus') {
+                if (peran == 'Tabib') {
+                    console.log(`"Selamat datang di Dunia Proxytia, ${nama}" \n "Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka." `)
+                } else {
+                    console.log('"Peran salah, silahkan coba lagi!"')
+                }
+            }else if (nama == 'Rendy') {
+                if (peran == 'Penyihir') {
+                    console.log(`"Selamat datang di Dunia Proxytia, ${nama}" \n Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
+                } else {
+                    console.log('"Peran salah, silahkan coba lagi!"')
+                }
+            }else{
+                console.log('"nama tidak ditemukan, silahkan coba lagi!"')
+            }   
+        } else {
+            console.log(`Hallo ${nama} Pilih peranmu untuk memulai game!`)
         }
+    }else{
+        console.log(' "Nama harus diisi" ');
     }
